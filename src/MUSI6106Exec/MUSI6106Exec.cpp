6f62fd04cbc475532e476525a6f5c6fa4685c55a
alexander lerch <alexander.lerch@gatech.edu>
2018-01-10 11:36:05 -0500

    initial commit of project template

---
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -0,0 +1,69 @@
+
+#include <iostream>
+#include <ctime>
+
+#include "MUSI8903Config.h"
+
+#include "AudioFileIf.h"
+
+using std::cout;
+using std::endl;
+
+// local function declarations
+void    showClInfo ();
+
+/////////////////////////////////////////////////////////////////////////////////
+// main function
+int main(int argc, char* argv[])
+{
+    std::string             sInputFilePath,                 //!< file paths
+                            sOutputFilePath;
+
+    long long               iInFileLength       = 0;        //!< length of input file
+
+    clock_t                 time                = 0;
+
+    float                   **ppfAudioData      = 0;
+
+    CAudioFileIf            *phAudioFile        = 0;
+
+    showClInfo ();
+
+    //////////////////////////////////////////////////////////////////////////////
+    // parse command line arguments
+
+    //////////////////////////////////////////////////////////////////////////////
+    // open the input wave file
+
+    //////////////////////////////////////////////////////////////////////////////
+    // allocate memory
+    time                    = clock();
+
+    // get audio data and write it to the output file
+
+
+    cout << "reading/writing done in: \t"    << (clock()-time)*1.F/CLOCKS_PER_SEC << " seconds." << endl;
+    //////////////////////////////////////////////////////////////////////////////
+    // get audio info and print it to stdout
+
+    //////////////////////////////////////////////////////////////////////////////
+    // do processing
+    cout << "Hello there!" << endl << endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // clean-up
+
+    return 0;
+    
+}
+
+
+void     showClInfo()
+{
+    cout << "GTCMT MUSI6106 Executable" << endl;
+    cout << "(c) 2014-2018 by Alexander Lerch" << endl;
+    cout  << endl;
+
+    return;
+}
+
